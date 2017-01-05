d29fce66ecd01ce93dde82c3936777030c3542d3
Roberto Osses <rosses@gmail.com>
2017-01-05 15:35:09 -0300

    1

--- www/js/app.js
+++ www/js/app.js
@@ -34,7 +34,7 @@ angular.module('growify', ['ngCordova', 'angular-websql', 'ionic', 'growify.cont
 
     setTimeout(function() {
         navigator.splashscreen.hide();
-    }, 100);
+    }, 3000);
     
   });
   $ionicPlatform.registerBackButtonAction(function(e){
